a3e953fcc659f67f90d21df7ff3fa428fefa9456
Seyma SARIGİL <seymasargil@gmail.com>
2022-07-26 15:10:19 +0300

    editted for publishing
    Co-Authored-By: Seyma SARIGİL <seymasargil@gmail.com>

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 import os
+import time
+
 from flask import Flask
+
+import ctor
 from models.models import db as models_db
 from views.views import tagging_operations
 
-if __name__ == "__main__":
-    app = Flask(__name__)
-    basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-    app.secret_key = "cokgizlibiranahtar"
+app.secret_key = "cokgizlibiranahtar"
 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = \
-    #    'sqlite:///' + os.path.join(basedir, 'database.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://scibriigaxirvh" \
-                                            ":701e929c57a1042d3ac1c53aa27a433ca0c15541329106abe1a012d00be8d533@ec2-107-22" \
-                                            "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf "
+#app.config['SQLALCHEMY_DATABASE_URI'] = \
+#    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://scibriigaxirvh" \
+                                        ":701e929c57a1042d3ac1c53aa27a433ca0c15541329106abe1a012d00be8d533@ec2-107-22" \
+                                        "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf "
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.register_blueprint(tagging_operations)
-    models_db.init_app(app)
+app.register_blueprint(tagging_operations)
+models_db.init_app(app)
 
-    with app.app_context():
-        models_db.create_all()
+ctor.dummy()
 
-    port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+with app.app_context():
+    models_db.create_all()
+
