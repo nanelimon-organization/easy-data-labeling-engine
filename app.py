4d401c22a9909259e3f358e3c9d510e1074c48a8
Seyma SARIGİL <seymasargil@gmail.com>
2022-07-26 14:45:05 +0300

    editing secret_key
    Co-Authored-By: Seyma SARIGİL <seymasargil@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from decouple import config
 import os
 from flask import Flask
 from models.models import db as models_db
@@ -7,10 +6,10 @@ from views.views import tagging_operations
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.secret_key = config('secret_key')
+app.secret_key = "cokgizlibiranahtar"
 
-"""app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'database.db')"""
+#app.config['SQLALCHEMY_DATABASE_URI'] = \
+#    'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://scibriigaxirvh" \
                                         ":701e929c57a1042d3ac1c53aa27a433ca0c15541329106abe1a012d00be8d533@ec2-107-22" \
                                         "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf "
