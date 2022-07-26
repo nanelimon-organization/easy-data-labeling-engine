59856217426f48361138f7c6c9e0e639cc7e0ae7
Seyma SARIGİL <seymasargil@gmail.com>
2022-07-26 21:45:45 +0300

    edited db section
    Co-Authored-By: Seyma SARIGİL <seymasargil@gmail.com>

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.secret_key = "cokgizlibiranahtar"
 
-"""app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'database.db')"""
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://scibriigaxirvh" \
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+"""app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://scibriigaxirvh" \
                                         ":701e929c57a1042d3ac1c53aa27a433ca0c15541329106abe1a012d00be8d533@ec2-107-22" \
-                                        "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf "
+                                        "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf """"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
