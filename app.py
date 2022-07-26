ae69e0fef4e4eefc5fbfab31fb08af885ac0e5e1
Seyma SARIGİL <seymasargil@gmail.com>
2022-07-26 12:04:52 +0300

    Updated database for publishing. Requirement.txt created.
    Updated database for publishing. Requirement.txt created.
    
    Co-Authored-By: Seyma SARIGİL <seymasargil@gmail.com>

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.secret_key = config('secret_key')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'database.db')
+"""app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')"""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://scibriigaxirvh" \
+                                        ":701e929c57a1042d3ac1c53aa27a433ca0c15541329106abe1a012d00be8d533@ec2-107-22" \
+                                        "-122-106.compute-1.amazonaws.com:5432/d9i8pr0r2k38vf "
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
