1c6d6e6a8da14fa767bd923ff12cd4c383ac45a8
Seyma SARIGİL <seymasargil@gmail.com>
2022-07-26 23:35:18 +0300

    Added dataset extraction feature
    Co-Authored-By: Seyma SARIGİL <seymasargil@gmail.com>

--- views/views.py
+++ views/views.py
@@ -1,12 +1,14 @@
-import datetime
-
+import pandas as pd
 from flask import Blueprint, render_template, redirect
 import getpass
 
-from models.models import Tagging, Scraped,db
+from sqlalchemy import create_engine
+
+from models.models import Tagging, Scraped
 
 tagging_operations = Blueprint('tagging_operations', __name__)
 user = getpass.getuser()
+
 
 @tagging_operations.route('/', methods=['GET'])
 def index():
@@ -19,19 +21,33 @@ def index():
 
 @tagging_operations.route('/bullying/<int:id>')
 def label_as_bully(id):
-    Scraped.label_update(id=id, label=True, tagging_status= True)
+    Scraped.label_update(id=id, label=True, tagging_status=True)
     Tagging.new_data_insert(scraped_id=id, tagger=getpass.getuser())
     return redirect('/')
 
 
 @tagging_operations.route('/not_bulling/<int:id>')
 def label_as_not_bully(id):
-    Scraped.label_update(id=id, label=False, tagging_status= True)
+    Scraped.label_update(id=id, label=False, tagging_status=True)
     Tagging.new_data_insert(scraped_id=id, tagger=getpass.getuser())
     return redirect('/')
+
 
 @tagging_operations.route('/delete/<int:id>')
 def delete(id):
     #Zorbalık yok ama Tagging datasına(yani nihayi datasete) dahil edilmeyecek.
-    Scraped.label_update(id=id, label=False, tagging_status= True)
+    Scraped.label_update(id=id, label=False, tagging_status=True)
     return redirect('/')
+
+
+@tagging_operations.route('/extract_dataset')
+def extract_dataset():
+    engine = create_engine(
+        'postgresql://tfebtxzxlgssjc:41e1741c907c8fa23d960f7c99b53cc83688da12337f4565dde9d9e51c96f899@ec2-54-225-234-165.compute-1.amazonaws.com:5432/da5jjovb79fk6g',
+        echo=True)
+    connection = engine.raw_connection()
+    query = "SELECT tagging.scraped_id, scraped.text, tagging.tagger, tagging.tagged_date, scraped.label FROM tagging INNER JOIN scraped ON scraped.id = tagging.scraped_id"
+    df = pd.read_sql(query ,con=connection, index_col="scraped_id")
+    df.to_csv('static/datas/dataset.csv')
+    return redirect('/')
+
